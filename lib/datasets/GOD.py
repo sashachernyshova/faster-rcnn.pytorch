9e15f7dba79747f8e657fc9e8ae946c2c346cd8a
Sasha Chernyshova <alexandrachernyshova07@gmail.com>
2021-02-17 21:22:31 +0300

    Change GOD's classes

--- lib/datasets/GOD.py
+++ lib/datasets/GOD.py
@@ -46,11 +46,7 @@ class GOD(imdb):
             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'GOD' + self._year)
         self._classes = ('__background__',  # always index 0
-                        #  'table')
-                         'graph','sketches','histogram','medical','natural','block',
-                         'flow','vector','scatter','3d','mask','box','confusion','bar','heat',
-                         'polar','figure','table','pie')
-                        # 'figure', 'table') #, 'equation')
+                         'table', 'figure', 'logo', 'signature', 'natural_image')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
